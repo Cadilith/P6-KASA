c449551268c0d3a3bf441df34c14abb0a344a8c3
Tiffanie Orsoni <tiffanie.orsoni@gmail.com>
2023-05-31 12:10:03 +0200

    Init project routes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './components/App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home/'
+import Apropos from './pages/Apropos/'
+import Header from './components/Header'
+import Estate from './pages/Estate'
+import Error from './components/Error'
+import Footer from './components/Footer'
+import Banner from './components/Banner'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <Router>
+      <Header />
+      <Banner />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/a-propos" element={<Apropos />} />
+        <Route path="/estate" element={<Estate />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+      <Footer />
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
