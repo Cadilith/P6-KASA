23cf6ecc6ab6184154bd68426c890b5a6b23b757
Tiffanie Orsoni <tiffanie.orsoni@gmail.com>
2023-06-07 10:38:37 +0200

    Collapse component without animation

--- src/components/Collapse/index.jsx
+++ src/components/Collapse/index.jsx
@@ -1,8 +1,20 @@
 import './collapse.css'
+import { useState } from 'react'
+import arrow from '../../assets/arrow.svg'
 
-function Collapse() {
+function Collapse(props) {
+  const [open, setOpen] = useState(false)
+  const toggle = () => {
+    setOpen(!open)
+  }
   return (
-    
+    <div className="collapse-container">
+      <div className="collapse-title" onClick={toggle} activeclassname="active">
+        <p>{props.collapseTitle}</p>
+        <img className="arrow" src={arrow} alt="Ouvrir l'article" />
+      </div>
+      {open && <div className="collapse-content">{props.children}</div>}
+    </div>
   )
 }
 
--- src/pages/Apropos/index.jsx
+++ src/pages/Apropos/index.jsx
@@ -1,4 +1,5 @@
 import Banner from '../../components/Banner/'
+import Collapse from '../../components/Collapse'
 import AproposBanner from '../../assets/banner-a-propos.png'
 import './apropos.css'
 
@@ -8,6 +9,17 @@ function Apropos() {
   return (
     <main>
       <Banner img={AproposBanner} className={banner} />
+      <div className="about-container">
+        <Collapse collapseTitle={'Fiabilité'}>
+          <div className="collapse-content">
+            <p>
+              Les annonces postées sur Kasa garantissent une fiabilité totale.
+              Les photos sont conformes aux logements, et toutes les
+              informations sont régulièrement vérifiées par nos équipes.
+            </p>
+          </div>
+        </Collapse>
+      </div>
     </main>
   )
 }
