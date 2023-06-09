38fa7512e1274cfb5bb3ed046dee3f0765d37e99
Tiffanie Orsoni <tiffanie.orsoni@gmail.com>
2023-06-09 18:05:41 +0200

    Caroussel animation ok

--- src/components/Carrousel/index.jsx
+++ src/components/Carrousel/index.jsx
@@ -13,6 +13,18 @@ const Carrousel = ({ slides }) => {
   function goToPrevSlide() {
     setCurrent(current === 0 ? length - 1 : current - 1)
   }
+  function setSlideDirection(index) {
+    if (current === length - 1 && index === 0) {
+      return 'next'
+    } else if (current === 0 && index === length - 1) {
+      return 'prev'
+    } else if (current > index && current !== 0) {
+      return 'prev'
+    } else if (current < index && current !== length - 1) {
+      return 'next'
+    }
+  }
+
   return (
     <div className="carrousel-container">
       <div className="carrousel-button-container">
@@ -27,18 +39,18 @@ const Carrousel = ({ slides }) => {
         return (
           <div
             className={
-              current === index ? 'carrousel-img current ' : 'carrousel-img '
+              current === index
+                ? 'carrousel-img current '
+                : 'carrousel-img ' + setSlideDirection(index)
             }
             key={index}
           >
-            {index === current && (
-              <>
-                <img src={slide} alt="" />
-                <p>
-                  {current + 1}/{length}
-                </p>
-              </>
-            )}
+            <>
+              <img src={slide} alt="" />
+              <p>
+                {current + 1}/{length}
+              </p>
+            </>
           </div>
         )
       })}
