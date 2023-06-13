169f940adb496e130862bb8607ee5fb1c7eea8a4
Tiffanie Orsoni <tiffanie.orsoni@gmail.com>
2023-06-13 14:15:45 +0200

    profile picture default image if error is found

--- src/components/ProfilePicture/index.jsx
+++ src/components/ProfilePicture/index.jsx
@@ -1,11 +1,16 @@
+import defaultImg from '../../assets/host.svg'
+
 function linebreak(text) {
   return text.replace(' ', '\n')
 }
 function ProfilePicture({ picture, name }) {
+  const imgErrorHandler = (e) => {
+    e.target.src = defaultImg
+  }
   return (
     <div className="profile">
       <p>{linebreak(name)}</p>
-      <img src={picture} alt={name} />
+      <img src={picture} alt={name} onError={(e) => imgErrorHandler(e)} />
     </div>
   )
 }
